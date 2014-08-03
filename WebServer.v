c30fa35552adaebb9165ce6ebe2bea2a966f36b1
Guillaume Claret <dev@clarus.me>
2014-08-04 01:07:07 +0200

    Example with the set function

--- WebServer.v
+++ WebServer.v
@@ -60,7 +60,7 @@ Module Env.
     channels : Signature.t }.
 End Env.
 
-Module Reference.
+Module Ref.
   Class C (A : Type) (env : Env.t) : Type := New {
     get : Memory.t (Env.references env) -> A;
     set : Memory.t (Env.references env) -> A -> Memory.t (Env.references env) }.
@@ -74,9 +74,9 @@ Module Reference.
     (I : C A (Env.New references channels)) : C A (Env.New (B :: references) channels) := {
     get mem := get (Memory.tail mem);
     set mem x := Memory.Cons (Memory.head mem) (set (Memory.tail mem) x) }.
-End Reference.
+End Ref.
 
-Module Writer.
+Module Out.
   Class C (A : Type) (env : Env.t) : Type := New {
     write : Output.t (Env.channels env) -> A -> Output.t (Env.channels env) }.
 
@@ -87,7 +87,7 @@ Module Writer.
   Instance cons_right (A B : Type) (references : Signature.t) (channels : Signature.t)
     (I : C A (Env.New references channels)) : C A (Env.New references (B :: channels)) := {
     write output x := Output.Cons (Output.head output) (write (Output.tail output) x) }.
-End Writer.
+End Out.
 
 Module C.
   Inductive t (env : Env.t) : Type -> Type :=
@@ -95,11 +95,11 @@ Module C.
     t env A
   | bind : forall (A B : Type),
     t env A -> (A -> t env B) -> t env B
-  | get : forall (A : Type), `{Reference.C A env} ->
+  | get : forall (A : Type), `{Ref.C A env} ->
     t env A
-  | set : forall (A : Type), `{Reference.C A env} ->
+  | set : forall (A : Type), `{Ref.C A env} ->
     A -> t env unit
-  | write : forall (A : Type), `{Writer.C A env} ->
+  | write : forall (A : Type), `{Out.C A env} ->
     A -> t env unit.
   Arguments ret [env A] _.
   Arguments bind [env A B] _ _.
@@ -117,9 +117,9 @@ Module C.
       match run_aux _ _ _ mem output x with
       | (x, mem, output) => run_aux _ _ _ mem output (f x)
       end
-    | get _ _ => (Reference.get mem, mem, output)
-    | set _ _ v => (tt, Reference.set mem v, output)
-    | write _ _ v => (tt, mem, Writer.write output v)
+    | get _ _ => (Ref.get mem, mem, output)
+    | set _ _ v => (tt, Ref.set mem v, output)
+    | write _ _ v => (tt, mem, Out.write output v)
     end.
 
   Definition run (references : Signature.t) (channels : Signature.t) (A : Type)
@@ -144,24 +144,39 @@ Module Test.
   Import C.Notations.
   Open Local Scope string.
 
-  Definition hello_world {env : Env.t} `{Writer.C string env}
+  Definition hello_world {env : Env.t} `{Out.C string env}
     (_ : unit) : C.t env unit :=
     do! C.write _ "Hello " in
     C.write _ "world!".
 
   Compute C.run [string : Type] Memory.Nil (hello_world tt).
 
-  Definition read_and_print {env : Env.t} `{Reference.C nat env} `{Writer.C nat env}
+  Definition read_and_print {env : Env.t} `{Ref.C nat env} `{Out.C nat env}
     (_ : unit) : C.t env unit :=
     let! n : nat := C.get _ in
     C.write _ n.
 
   Compute C.run [nat : Type] (Memory.Cons 12 Memory.Nil) (read_and_print tt).
 
-  Definition hello_read_print {env : Env.t} `{Reference.C nat env} `{Writer.C string env} `{Writer.C nat env}
+  Definition hello_read_print {env : Env.t} `{Ref.C nat env} `{Out.C string env} `{Out.C nat env}
     (_ : unit) : C.t env unit :=
     do! hello_world tt in
     read_and_print tt.
 
   Compute C.run [nat : Type; string : Type] (Memory.Cons 12 Memory.Nil) (hello_read_print tt).
+
+  Definition incr_by {env : Env.t} `{Ref.C nat env}
+    (n : nat) : C.t env unit :=
+    let! m : nat := C.get _ in
+    C.set _ (m + n).
+
+  Definition double_print {env : Env.t} `{Ref.C nat env} `{Out.C nat env}
+    (n : nat) : C.t env unit :=
+    do! C.set _ 0 in
+    do! incr_by n in
+    do! incr_by n in
+    let! n : nat := C.get _ in
+    C.write _ n.
+
+  Compute C.run [nat : Type] (Memory.Cons 15 Memory.Nil) (double_print 12).
 End Test.
